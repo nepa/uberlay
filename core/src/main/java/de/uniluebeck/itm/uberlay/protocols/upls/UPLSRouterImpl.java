8f934d3f2a1ababab3007ecfa94d85319035a949
Sascha Seidel <seidels@informatik.uni-luebeck.de>
2011-06-07 14:25:52 +0200

    UPLS Routing begonnen.

--- core/src/main/java/de/uniluebeck/itm/uberlay/protocols/upls/UPLSRouterImpl.java
+++ core/src/main/java/de/uniluebeck/itm/uberlay/protocols/upls/UPLSRouterImpl.java
@@ -1,16 +1,28 @@
 package de.uniluebeck.itm.uberlay.protocols.upls;
 
+import org.jboss.netty.channel.ChannelDownstreamHandler;
+import org.jboss.netty.channel.ChannelEvent;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelUpstreamHandler;
+import org.jboss.netty.channel.DownstreamMessageEvent;
+import org.jboss.netty.channel.UpstreamMessageEvent;
+
 import com.google.inject.Singleton;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+
 import de.uniluebeck.itm.uberlay.protocols.up.UP;
-import org.jboss.netty.channel.*;
+import de.uniluebeck.itm.uberlay.protocols.up.UP.UPPacket;
 
 @Singleton
 public class UPLSRouterImpl implements ChannelUpstreamHandler, ChannelDownstreamHandler, UPLSRouter {
+	private UPLSRoutingTable uplsRoutingTable = new UPLSRoutingTable();
 
 	@Override
 	public void handleUpstream(final ChannelHandlerContext ctx, final ChannelEvent e) throws Exception {
 		if (e instanceof UpstreamMessageEvent && ((UpstreamMessageEvent) e).getMessage() instanceof UPLS.UPLSPacket) {
-			route((UPLS.UPLSPacket) ((UpstreamMessageEvent) e).getMessage(), e.getFuture());
+			route(ctx, (UPLS.UPLSPacket) ((UpstreamMessageEvent) e).getMessage(), e.getFuture());
 		} else {
 			ctx.sendUpstream(e);
 		}
@@ -21,19 +33,38 @@ public class UPLSRouterImpl implements ChannelUpstreamHandler, ChannelDownstream
 		if (e instanceof DownstreamMessageEvent && ((DownstreamMessageEvent) e).getMessage() instanceof UP.UPPacket) {
 			final UP.UPPacket upPacket = (UP.UPPacket) ((DownstreamMessageEvent) e).getMessage();
 			final UPLS.UPLSPacket uplsPacket = encapsulatePacket(upPacket);
-			route(uplsPacket, e.getFuture());
+			route(ctx, uplsPacket, e.getFuture());
 		} else {
 			ctx.sendDownstream(e);
 		}
 	}
 
 	@Override
-	public void route(final UPLS.UPLSPacket packet, final ChannelFuture callerFuture) {
-		// TODO implement
+	public void route(ChannelHandlerContext context, final UPLS.UPLSPacket packet, final ChannelFuture callerFuture) {
+		final int destinationLabel = packet.getLabel();
+		final int localLabel = this.uplsRoutingTable.getLocalLabel();
+		
+		if (destinationLabel == localLabel)
+		{
+			try {
+				UP.UPPacket upPacket;
+				upPacket = UPPacket.parseFrom(packet.getPayload());			
+				context.sendUpstream(new UpstreamMessageEvent(context.getChannel(), upPacket, null));				
+			}
+			catch (InvalidProtocolBufferException e)
+			{
+				callerFuture.setFailure(e);
+			}
+
+		}		
 	}
 
 	private UPLS.UPLSPacket encapsulatePacket(final UP.UPPacket message) {
-		// TODO implement
-		return null;
+		int uplsLabel = this.uplsRoutingTable.lookupLabel(message.getDestination());
+		
+		return UPLS.UPLSPacket.newBuilder()
+			.setPayload(ByteString.copyFrom(message.toByteArray()))
+			.setLabel(uplsLabel)
+			.build();
 	}
 }
--- core/src/main/java/de/uniluebeck/itm/uberlay/protocols/upls/UPLSRoutingTable.java
+++ core/src/main/java/de/uniluebeck/itm/uberlay/protocols/upls/UPLSRoutingTable.java
@@ -4,5 +4,13 @@ package de.uniluebeck.itm.uberlay.protocols.upls;
  * Created by IntelliJ IDEA. User: bimschas Date: 31.05.11 Time: 17:58 TODO change
  */
 public class UPLSRoutingTable {
-
+	public int lookupLabel(final String destinationAddress)
+	{
+		return 0; // TODO Lookup label
+	}
+	
+	public int getLocalLabel()
+	{
+		return 0; // TODO Return local label
+	}
 }
