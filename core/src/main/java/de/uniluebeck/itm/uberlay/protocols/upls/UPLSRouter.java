110f858b9af01808b1847fdf90f6038056c81f20
Sascha Seidel <seidels@informatik.uni-luebeck.de>
2011-06-07 14:30:29 +0200

    Signatur korrigiert.

--- core/src/main/java/de/uniluebeck/itm/uberlay/protocols/upls/UPLSRouter.java
+++ core/src/main/java/de/uniluebeck/itm/uberlay/protocols/upls/UPLSRouter.java
@@ -1,10 +1,11 @@
 package de.uniluebeck.itm.uberlay.protocols.upls;
 
 import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelUpstreamHandler;
 
 public interface UPLSRouter extends ChannelUpstreamHandler {
 
-	void route(UPLS.UPLSPacket packet, final ChannelFuture callerFuture);
+	void route(ChannelHandlerContext context, UPLS.UPLSPacket packet, final ChannelFuture callerFuture);
 
 }
